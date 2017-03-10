dd7dff8db7ee80c3ff342b9fbda330835e9276c5
Kayla Astl <kaylaastl@Kaylas-MacBook-Pro.local>
2017-03-10 12:18:32 -0800

    TeamTimbre

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -11,6 +11,7 @@
 
   // Create a variable to reference the database
  var database = firebase.database();
+ //
 
 
 function launchSearch(){
@@ -18,11 +19,13 @@ function launchSearch(){
   $("#launch-search").on("click",function(){
 
   //url request to the eventful API(this will access our events)
-  var eventfulURL ="http://api.eventful.com/json/events/search?app_key=2CH4skmC8kN48Lr4?q=music&l=92109&within=10&units=miles";
+  var eventfulURL ="https://api.eventful.com/json/events/search?app_key=2CH4skmC8kN48Lr4?q=music&l=92109&within=10&units=miles";
   
   //grab our user input by building an onclick submit to the API
+      //must reference the parameters: location by zip?, music, 
+
   //create our dropdown menu access
-    //1. create variables to hold our San Diego regions 
+    //1. create variables to hold/call our San Diego regions >DEFINE these how??? 
         //construct this selection
     //2. on select we will send our request to the API
 
@@ -30,16 +33,35 @@ function launchSearch(){
 	   $.ajax({
    		url: eventfulURL,
    		method: "GET"
-   	  })
-   
+   	  })  
 //.done function will run what we need to run after we get the API data
 		    .done(function(response) {
 		    console.log(response);
 //in here put all the gif display/reporting that will happen
 		    var results  = response.data 
-
         console.log(results);
+
+//create a loop to iterate through out return
+        //run through the results page
+        for (var i = 0; i < results.length; i++) {
+
+          
+
+        }
+
+//I want to return each search result as an eventObject
+        var eventObject ={
+          date: "",
+          artist:"",
+          venue:"",
+          eventLink:"",
+          
+          artistImg:"",
+          songLink:""
+        }
+
+
 		    });
-    });
+  });
 }
 launchSearch();
