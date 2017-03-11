dfe11284171c5d76ab55d910873b47adf2a52c96
Kayla Astl <kaylaastl@Kaylas-MacBook-Pro.local>
2017-03-11 13:19:41 -0800

    TeamTimbre

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -11,7 +11,90 @@
 
   // Create a variable to reference the database
  var database = firebase.database();
- //
+
+
+
+var gData = [];
+
+
+
+
+//ACCESSING THE API
+//i dont know what this is?
+function show_alert(){
+  var oArgs = {
+            app_key:"2CH4skmC8kN48Lr4",
+            id: "20218701",
+            page_size: 10,
+  };
+  EVDB.API.call("/events/get", oArgs, function(oData) {
+      // Note: this relies on the custom toString() methods below
+    });
+}
+function clickBtn()
+{
+   var oArgs = {
+      app_key: "2CH4skmC8kN48Lr4",
+      q: "music",
+      where: "Chula Vista", 
+      //postal_code:"92109",
+      page_size: 10,
+      sort_order: "popularity",
+   };
+
+   EVDB.API.call("/events/search", oArgs, function(oData) {
+    var app = oData;
+    console.log(app)
+    console.log(app.events.event[0].city_name);
+    console.log(app.events.event[0].venue_name);
+    console.log(app.events.event[0].postal_code);
+    console.log(app.total_items); 
+    
+
+
+    for (var i = 0; i < app.total_items - 1; i++) {
+      //QA
+      if (app.events.event[i].city_name === null)
+
+    //my event object information
+    //var eventListing 
+
+    gData.push({
+    city: app.events.event[i].city_name,
+    venue: app.events.event[i].venue_name,
+    title: app.events.event[i].title,
+    //artist: app.events.event[i].performers.performer.name,//this goes to itunes
+    when: app.events.event[i].start_time,
+    //url: app.events.event[0].url,
+    imageStr: app.events.event[i].image.medium.url});
+
+  
+
+    console.log(gData[i]);
+
+
+    $("#test").html("<br>Event Location : " + gData[i].city + "<br>" );
+    
+    $("#test1").html(gData[i].when);
+    $("#test2").html(gData[i].title);
+    $("#test3").html(gData[i].venue);
+    //$("#test4").html("<br>" + eventListing.url + "<br>" );
+    $("#test5").append("<img src='" + gData[i].imageStr + "' width='180' height='180'>");
+   
+    //}
+      // Note: this relies on the custom toString() methods below
+
+    }
+
+    });
+
+}
+
+
+
+
+
+
 
 
 function launchSearch(){
