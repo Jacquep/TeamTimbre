44d2e2afc6711bcf16745924449c84bdf2a46975
Jacqueline Palmer <jackiemariep@yahoo.com>
2017-12-10 16:56:43 -0800

    reconfigured firebase

--- assets/javascript/index-style.js
+++ assets/javascript/index-style.js
@@ -1,220 +1,205 @@
 // Initialize Firebase
 var config = {
-   apiKey: "AIzaSyCWd9bfk2Z2wvy4nU3o3ahdJpO9KLTvayo",
-   authDomain: "timbre-ee3e8.firebaseapp.com",
-   databaseURL: "https://timbre-ee3e8.firebaseio.com",
-    storageBucket: "timbre-ee3e8.appspot.com",
-   messagingSenderId: "457573350640"
- };
-
-firebase.initializeApp(config);
-
+  apiKey: 'AIzaSyCax75le3dd4yc1D8Mz5gHxhGRh48e_zic',
+  authDomain: 'timbre-4e4bc.firebaseapp.com',
+  databaseURL: 'https://timbre-4e4bc.firebaseio.com',
+  projectId: 'timbre-4e4bc',
+  storageBucket: 'timbre-4e4bc.appspot.com',
+  messagingSenderId: '988935523367'
+}
+firebase.initializeApp(config)
 
 // Create a variable to reference the database
-var database = firebase.database();
+var database = firebase.database()
 
 // Create variable for current user data
-var currentUser = {};
+var currentUser = {}
 
 // Created variable to store current user's unique ID#
-var userId = currentUser.uid;
+var userId = currentUser.uid
 
-console.log(currentUser.uid);
-console.log(firebase.auth().currentUser);
+console.log(currentUser.uid)
+console.log(firebase.auth().currentUser)
 
-firebase.auth().signOut().then(function() {
-
-// Sign-out successful.
-        }).catch(function(error) {
+firebase
+  .auth()
+  .signOut()
+  .then(function() {
+    // Sign-out successful.
+  })
+  .catch(function(error) {
     // An error happened.
-    });
+  })
 
 //-----------DISABLED FX-----------------
 //function checkEmail() {
 
-  // var email = $("#Email").val();
-  // var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+// var email = $("#Email").val();
+// var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
 
-  // if (!filter.test(email.value)) {
-  // $("#errorMsg").show().text('Please provide a valid email address');
- 
-  // return false;
-  // }
+// if (!filter.test(email.value)) {
+// $("#errorMsg").show().text('Please provide a valid email address');
+
+// return false;
+// }
 //} ------- END DISABLED FX ------------
-
 
 // A function for logging out the user
 function logOutUser() {
+  event.preventDefault()
 
-  event.preventDefault();
-    
   firebase.auth().signOut().then(function() {
-    
     // Sign-out successful.
 
-    $("#user-options").hide();
-    $("#pageload-buttons").show()
-    $("#errorMsg").show().text('You Have Logged Out! Sign in Again above.');
-    $("#regional-dropdown").hide();
-
-    });
-  }
+    $('#user-options').hide()
+    $('#pageload-buttons').show()
+    $('#errorMsg').show().text('You Have Logged Out! Sign in Again above.')
+    $('#regional-dropdown').hide()
+  })
+}
 
 //moving this to the firebase storage
 $(document).ready(function() {
-  
- $("#area-of-interest").on("click", function () {
- 
-   var userRegion = $('#area-of-interest option:selected').text();
-   console.log(userRegion);
+  $('#area-of-interest').on('click', function() {
+    var userRegion = $('#area-of-interest option:selected').text()
+    console.log(userRegion)
 
-   var currentUser = firebase.auth().currentUser.uid;
+    var currentUser = firebase.auth().currentUser.uid
 
-   if (userRegion !== "Select your Region of Interest") {
-
-     window.location.href = 'results.html' + '#' + userRegion + '#' + currentUser;
-
-   }
-
- });
+    if (userRegion !== 'Select your Region of Interest') {
+      window.location.href =
+        'results.html' + '#' + userRegion + '#' + currentUser
+    }
+  })
 
   //login information
-  $("#validate-user-btn").on("click", function (event) {
-       
+  $('#validate-user-btn').on('click', function(event) {
     //var userName= $("#name").val();
-      
-    event.preventDefault();
 
-    var email = $("#email-login").val();
+    event.preventDefault()
 
-    var password = $("#password-login").val();
-      
-    firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+    var email = $('#email-login').val()
 
-      // Handle Errors here.  
+    var password = $('#password-login').val()
 
-      if (error) {
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .catch(function(error) {
+        // Handle Errors here.
 
-        var errorCode = error.code;
-        console.log(errorCode);
+        if (error) {
+          var errorCode = error.code
+          console.log(errorCode)
 
-        var errorMessage = error.message;
-        console.log(errorMessage);
-      
-        $("#errorMsg").show().text('User Not Found! Please Sign Up or Try Again');
-      }
+          var errorMessage = error.message
+          console.log(errorMessage)
 
-    }).then(function(user) {
-  
-      if (user) {
-        // user authenticated with Firebase
-        currentUser = user;
-        console.log(currentUser);
+          $('#errorMsg')
+            .show()
+            .text('User Not Found! Please Sign Up or Try Again')
+        }
+      })
+      .then(function(user) {
+        if (user) {
+          // user authenticated with Firebase
+          currentUser = user
+          console.log(currentUser)
 
-        $("#user-options").show();
-        $("#login-form").hide();
-        $("#random-lyric").hide();
-        $("#regional-dropdown").show();
+          $('#user-options').show()
+          $('#login-form').hide()
+          $('#random-lyric').hide()
+          $('#regional-dropdown').show()
+        } else {
+          // user is logged out
+        }
+      })
+  })
 
-      } else {
-        // user is logged out
-      }
-
-    });
-
-  });
-
-  $("#log-out").on("click", logOutUser);
+  $('#log-out').on('click', logOutUser)
 
   //Create User
 
-  $("#create-user").on("click", function (event) {
-    
-    event.preventDefault();
+  $('#create-user').on('click', function(event) {
+    event.preventDefault()
 
-    userData = {};
-    
-    var email = $("#email-new").val();
-    var password = $("#password-new").val();
-       
-    var areaInt = $("#area-of-interest").val();
-    var name =  $("#name").val();
+    userData = {}
+
+    var email = $('#email-new').val()
+    var password = $('#password-new').val()
+
+    var areaInt = $('#area-of-interest').val()
+    var name = $('#name').val()
 
     //userData.interest = $("#area-of-interest").val();
-    userData.name = $("#name").val();
-    userData.list = "";
-    userData.password = password;
+    userData.name = $('#name').val()
+    userData.list = ''
+    userData.password = password
 
-    setData = {};
+    setData = {}
 
-    var userId =  currentUser.uid;
+    var userId = currentUser.uid
 
-    console.log(userId);
-    console.log(userData);
-    
-    firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
-  
-      // Handle Errors here.
-  
-      if (error) {
+    console.log(userId)
+    console.log(userData)
 
-        var errorCode = error.code;
-        var errorMessage = error.message;
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .catch(function(error) {
+        // Handle Errors here.
 
-        console.log(errorCode);
-        console.log(errorMessage);
+        if (error) {
+          var errorCode = error.code
+          var errorMessage = error.message
 
-        if (errorMessage === "The email address is badly formatted.") {
-          
-          $("#email-validate").text("Please provide a valid e-mail address.");
+          console.log(errorCode)
+          console.log(errorMessage)
 
-        } else if (errorMessage === "The email address is already in use by another account.") {
-
-          $("#email-validate").text("The email address is already in use.");
-          
-        } else if (errorMessage === "Password should be at least 6 characters") {
-          
-          $("#password-validate").text('Try again. Password must be at least 6 characters long.').css("color","#ff0000");
+          if (errorMessage === 'The email address is badly formatted.') {
+            $('#email-validate').text('Please provide a valid e-mail address.')
+          } else if (
+            errorMessage ===
+            'The email address is already in use by another account.'
+          ) {
+            $('#email-validate').text('The email address is already in use.')
+          } else if (
+            errorMessage === 'Password should be at least 6 characters'
+          ) {
+            $('#password-validate')
+              .text('Try again. Password must be at least 6 characters long.')
+              .css('color', '#ff0000')
+          }
         }
-      }
+      })
+      .then(function(user) {
+        if (user) {
+          // user authenticated with Firebase
+          currentUser = user
+          console.log(currentUser)
+          var currentKey = currentUser.uid
 
-  }).then(function(user) {
+          var fbPath = 'users/' + currentKey + '/'
 
-    if (user) {
-      // user authenticated with Firebase
-      currentUser = user;
-      console.log(currentUser);
-      var currentKey = currentUser.uid;
+          $('#user-options').show()
+          $('#pageload-buttons').hide()
 
-      var fbPath = "users/" + currentKey + "/";
+          $('#regional-dropdown').show()
 
-      $("#user-options").show();
-      $("#pageload-buttons").hide();
-      
-      $("#regional-dropdown").show();
+          // $('#create-user').submit(function(e) {
+          //   e.preventDefault();
 
-      // $('#create-user').submit(function(e) {
-      //   e.preventDefault();
+          //   // Hide Modal after signup (@TODO: Prevent for hiding if fields generate errors)
+          //   // $('#myModal').modal('toggle'); //or  $('#IDModal').modal('hide');
+          //   // return false;
+          // });
 
-      //   // Hide Modal after signup (@TODO: Prevent for hiding if fields generate errors)
-      //   // $('#myModal').modal('toggle'); //or  $('#IDModal').modal('hide');
-      //   // return false;
-      // });
+          $('#myModal').modal('hide')
 
-      $('#myModal').modal('hide');
-
-      database.ref(fbPath).update(userData);
-      
-    } else {
-
-    // user is logged out
-    }
-
-
-  });
-
-
-});
-
-
-});
+          database.ref(fbPath).update(userData)
+        } else {
+          // user is logged out
+        }
+      })
+  })
+})
--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,148 +1,122 @@
-  // Initialize Firebase
-  //Brian's the man but so is Kayla
- // var config = {
-   // apiKey: "AIzaSyCWd9bfk2Z2wvy4nU3o3ahdJpO9KLTvayo",
-   // authDomain: "timbre-ee3e8.firebaseapp.com",
-   // databaseURL: "https://timbre-ee3e8.firebaseio.com",
-   // storageBucket: "timbre-ee3e8.appspot.com",
-   // messagingSenderId: "457573350640"
-  //};
- // firebase.initializeApp(config);
+// Initialize Firebase
+//Brian's the man but so is Kayla
+// var config = {
+// apiKey: "AIzaSyCWd9bfk2Z2wvy4nU3o3ahdJpO9KLTvayo",
+// authDomain: "timbre-ee3e8.firebaseapp.com",
+// databaseURL: "https://timbre-ee3e8.firebaseio.com",
+// storageBucket: "timbre-ee3e8.appspot.com",
+// messagingSenderId: "457573350640"
+//};
+// firebase.initializeApp(config);
 
-  // Create a variable to reference the database
- //var database = firebase.database();
-
-
+// Create a variable to reference the database
+//var database = firebase.database();
 
 var gData = [];
 
-
-
-
 //ACCESSING THE API
 //i dont know what this is?
-function show_alert(){
+function show_alert() {
   var oArgs = {
-            app_key:"2CH4skmC8kN48Lr4",
-            id: "20218701",
-            page_size: 10,
+    app_key: '2CH4skmC8kN48Lr4',
+    id: '20218701',
+    page_size: 10,
   };
-  EVDB.API.call("/events/get", oArgs, function(oData) {
-      // Note: this relies on the custom toString() methods below
+  EVDB.API.call('/events/get', oArgs, function(oData) {
+    // Note: this relies on the custom toString() methods below
   });
 }
-function clickBtn(){
-   var oArgs = {
-      app_key: "2CH4skmC8kN48Lr4",
-      q: "music",
-      where: "Solana Beach", 
-      //postal_code:"92109",
-      page_size: 10,
-      sort_order: "popularity",
-   };
+function clickBtn() {
+  var oArgs = {
+    app_key: '2CH4skmC8kN48Lr4',
+    q: 'music',
+    where: 'Solana Beach',
+    //postal_code:"92109",
+    page_size: 10,
+    sort_order: 'popularity',
+  };
 
-   EVDB.API.call("/events/search", oArgs, function(oData) {
+  EVDB.API.call('/events/search', oArgs, function(oData) {
     var app = oData;
-    console.log(app)
+    console.log(app);
     console.log(app.events.event[0].city_name);
     console.log(app.events.event[0].venue_name);
     console.log(app.events.event[0].postal_code);
-    console.log(app.total_items); 
-    
+    console.log(app.total_items);
 
-for (var i = 0; i < app.total_items - 1; i++) {
+    for (var i = 0; i < app.total_items - 1; i++) {
       //QA
       //if (app.events.event[i].city_name === null)
 
-    gData.push({
-    city: app.events.event[i].city_name,
-    venue: app.events.event[i].venue_name,
-    title: app.events.event[i].title,
-    //artist: app.events.event[i].performers.performer.name,//this goes to itunes
-    when: app.events.event[i].start_time,
-    //url: app.events.event[0].url,
-    imageStr: app.events.event[i].image.medium.url});
+      gData.push({
+        city: app.events.event[i].city_name,
+        venue: app.events.event[i].venue_name,
+        title: app.events.event[i].title,
+        //artist: app.events.event[i].performers.performer.name,//this goes to itunes
+        when: app.events.event[i].start_time,
+        //url: app.events.event[0].url,
+        imageStr: app.events.event[i].image.medium.url,
+      });
 
-  
-
-    console.log(gData[i]);
+      console.log(gData[i]);
     }
-    $("#test").html("<br>Event Location : " + gData[i].city + "<br>" );
-    
-    $("#test1").html(gData[i].when);
-    $("#test2").html(gData[i].title);
-    $("#test3").html(gData[i].venue);
+    $('#test').html('<br>Event Location : ' + gData[i].city + '<br>');
+
+    $('#test1').html(gData[i].when);
+    $('#test2').html(gData[i].title);
+    $('#test3').html(gData[i].venue);
     //$("#test4").html("<br>" + eventListing.url + "<br>" );
-    $("#test5").append("<img src='" + gData[i].imageStr + "' width='180' height='180'>");
-   
+    $('#test5').append(
+      "<img src='" + gData[i].imageStr + "' width='180' height='180'>",
+    );
+
     //}
-      // Note: this relies on the custom toString() methods below
-
-
-
+    // Note: this relies on the custom toString() methods below
   });
-
 }
 
-
-
-
-
-
-
-
-function launchSearch(){
+function launchSearch() {
   //
-  $("#launch-search").on("click",function(){
+  $('#launch-search').on('click', function() {
+    //url request to the eventful API(this will access our events)
+    var eventfulURL =
+      'http://api.eventful.com/json/events/search?app_key=2CH4skmC8kN48Lr4&q=music';
 
-  //url request to the eventful API(this will access our events)
-  var eventfulURL ="http://api.eventful.com/json/events/search?app_key=2CH4skmC8kN48Lr4&q=music"
-  
-  //grab our user input by building an onclick submit to the API
-      //must reference the parameters: location by zip?, music, 
+    //grab our user input by building an onclick submit to the API
+    //must reference the parameters: location by zip?, music,
 
-  //create our dropdown menu access
-    //1. create variables to hold/call our San Diego regions >DEFINE these how??? 
-        //construct this selection
+    //create our dropdown menu access
+    //1. create variables to hold/call our San Diego regions >DEFINE these how???
+    //construct this selection
     //2. on select we will send our request to the API
 
-
-
-
-
-//run ajax to send a request to the eventful server
-	   $.ajax({
-   		url: eventfulURL,
-   		method: "GET"
-   	  })  
-//.done function will run what we need to run after we get the API data
-		    .done(function(response) {
-		    console.log(response);
-//in here put all the gif display/reporting that will happen
-		    var results  = response.data 
+    //run ajax to send a request to the eventful server
+    $.ajax({
+      url: eventfulURL,
+      method: 'GET',
+    })
+      //.done function will run what we need to run after we get the API data
+      .done(function(response) {
+        console.log(response);
+        //in here put all the gif display/reporting that will happen
+        var results = response.data;
         console.log(results);
 
-//create a loop to iterate through out return
+        //create a loop to iterate through out return
         //run through the results page
-        for (var i = 0; i < results.length; i++) {
+        for (var i = 0; i < results.length; i++) {}
 
+        //I want to return each search result as an eventObject
+        var eventDate = {
+          date: '',
+          artist: '',
+          venue: '',
+          eventLink: '',
 
-
-        }
-
-//I want to return each search result as an eventObject
-        var eventObject ={
-          date: "",
-          artist:"",
-          venue:"",
-          eventLink:"",
-          
-          artistImg:"",
-          songLink:""
-        }
-
-
-		    });
+          artistImg: '',
+          songLink: '',
+        };
+      });
   });
 }
 launchSearch();
